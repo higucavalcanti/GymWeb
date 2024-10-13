4900578a7197da43ec2a842e7d35b4981850859f
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2024-10-13 15:53:51 -0300

    feat: creating project

---
+++ src/main/java/com/project/gymweb/GymWebApplication.java
@@ -0,0 +1,13 @@
+package com.project.gymweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/GymWeb/GymWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.GymWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
